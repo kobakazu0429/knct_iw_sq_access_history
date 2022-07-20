4091744d6b0bea7999afdd0ec496e4ee095de4c4
kobakazu0429 <kobakazu10@gmail.com>
2022-07-20 11:42:00 +0900

    fix

--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
 const verifyId = (id) => {
   // バーコード
-  // 学生証: 学生番号7桁 (1|2|3|4 年度(20\d{2}) 連番(\d{2})) + 0
+  // 学生証: 学生番号7桁 (1|2|3|4|8 年度(20\d{2}) 連番(\d{2})) + 0
   // 教職員: 7桁         0 0[1-5 9]0 \d{3}                   + 0
   //                         1:M, 2:E, 3:C, 4:A, 5:一般分野(人文社会 + 自然科学), 9:事務
-  const isStudent = /^[1-4]20\d{2}\d{2}0$/.test(id);
+  const isStudent = /^[1-48]20\d{2}\d{2}0$/.test(id);
   const isTeacherOrStaff = /^00[1-59]0\d{3}0$/.test(id);
   const verify = isStudent || isTeacherOrStaff;
   return verify;
